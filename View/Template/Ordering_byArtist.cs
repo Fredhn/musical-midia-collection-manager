6332d708945331cd6f7d3af59bffb7f4ba2e80f0
Fred Neves <fred_div@hotmail.com>
2017-06-07 00:56:44 -0300

    Add files via upload

---
+++ View/Template/Ordering_byArtist.cs
@@ -0,0 +1,22 @@
+﻿using AlbumCollectionManager.Model.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlbumCollectionManager.Template
+{
+    class Ordering_byArtist : OrderingTemplate
+    {
+        public override bool isPrimeiro(ListViewItem album1, ListViewItem album2)
+        {
+            if (String.Compare(album1.SubItems[1].Text, album2.SubItems[1].Text, true) <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ View/Template/Ordering_byYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlbumCollectionManager.Model.Core;
+using System.Windows.Forms;
+
+namespace AlbumCollectionManager.Template
+{
+    public class Ordering_byYear : OrderingTemplate
+    {
+        public override bool isPrimeiro(ListViewItem album1, ListViewItem album2)
+        {
+            Console.WriteLine("Ok Two!");
+            int int1 = (int.Parse(album1.SubItems[3].Text));
+            int int2 = (int.Parse(album2.SubItems[3].Text));
+            //var codigo = listView_pnlCollections_Albuns.Items[listView_pnlCollections_Albuns.SelectedItems[0].Index].SubItems[4];
+            if ((int.Parse(album1.SubItems[3].Text)) < (int.Parse(album2.SubItems[3].Text)))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
