ae2a00310c03c83b9551ac17018ec71561945eb1
Fred Neves <fred_div@hotmail.com>
2017-06-07 01:03:37 -0300

    Add files via upload

---
+++ Control/ListViewAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using AlbumCollectionManager.Control.DataAccess;
+
+namespace AlbumCollectionManager.Control.Adapter
+{
+    public abstract class ListViewAdapter
+    {
+        protected DataTable dt;
+        protected ListViewItem[] listitem;
+        protected Midia_DAO dao;
+
+        public ListViewAdapter ()
+        {
+
+        }
+
+        public abstract ListViewItem[] BuildItemsList();
+
+        public abstract DataRow BuildTextBoxData(int codigo);
+    }
+}
