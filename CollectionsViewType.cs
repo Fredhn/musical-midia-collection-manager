c1d6aeae57dadf8458e7d6359cf36298185d05d9
Fred Neves <fred_div@hotmail.com>
2017-06-07 00:57:22 -0300

    Add files via upload

---
+++ CollectionsViewType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumCollectionManager.Model.Type
+{
+    public enum CollectionsViewType
+    {
+        /// <summary>
+        /// Cassete Tape
+        /// </summary>
+        TAPE,
+        /// <summary>
+        /// Video Tape
+        /// </summary>
+        VCR,
+        /// <summary>
+        /// Vinyl Disc
+        /// </summary>
+        VINYL,
+        /// <summary>
+        /// Compact Disc
+        /// </summary>
+        CD,
+        /// <summary>
+        /// DVD Disc
+        /// </summary>
+        DVD,
+        /// <summary>
+        /// Blu-Ray Disc
+        /// </summary>
+        BLURAY,
+        /// <summary>
+        /// Blu-Ray Disc
+        /// </summary>
+        ALL
+    }
+}
