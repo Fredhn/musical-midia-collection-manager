2d73f213ee7f158e6222d89a14bcd204102dc88a
Fred Neves <fred_div@hotmail.com>
2017-06-07 00:58:12 -0300

    Add files via upload

---
+++ Model/Type/GenreType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumCollectionManager.Model.Type
+{
+    public enum GenreType
+    {
+        ROCK,
+        METAL,
+        REGGAE,
+        MPB,
+        POP,
+        PUNK,
+        SAMBA,
+        FORRO,
+        SERTANEJO,
+        SERTANEJO_UNI,
+        CLASSIC,
+        JAZZ,
+        BOSSA_NOVA,
+        RAP,
+        HIP_HOP,
+        BLUES,
+        COUNTRY,
+        FOLK,
+        ELETRONICA,
+        GOSPEL,
+        SOUL,
+        TROPICAL,
+        FADO,
+        AXE,
+        TRIBAL,
+        FUNK,
+        TRILHA_SONORA,
+        OTHER
+    }
+}
---
+++ Model/Type/OrderingCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumCollectionManager.Model.Type
+{
+    public enum OrderingCategory
+    {
+        Nome_Album,
+        Artista,
+        Ano_Lancamento,
+        Gênero
+    }
+}
